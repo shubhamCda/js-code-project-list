5a9687ecc42f873b951fb845bcea3228d63355a4
shubhamCda <shubhambodalkar281@gmail.com>
2025-08-23 16:22:10 +0530

    Add unique Accordion functionality at a time one ans visible

--- accordian/script.js
+++ accordian/script.js
@@ -3,18 +3,27 @@ const accordian = document.querySelectorAll(".accordian");
 console.log(accordian);
 
 accordian.forEach((accord) => {
-  let icon = accord.querySelector(".icon");
+	let icon = accord.querySelector(".icon");
   let answer = accord.querySelector(".answer");
+  let question = accord.querySelector(".question");
 
-  accord.addEventListener("click", () => {
-    if (icon.classList.contains("active")) {
-      icon.classList.remove("active");
-      answer.style.maxHeight = null;
-      
-    } else {
-      icon.classList.add("active");
-      answer.style.maxHeight = answer.scrollHeight + "px";
-      // console.log(answer.innerText);
-    }
-  })
-})
+	accord.addEventListener("click", () => {
+		accordian.forEach((element) => {
+      if (element !== accord) {
+        let icons = element.querySelector(".icon");
+        let answers = element.querySelector(".answer");
+				icons.classList.remove("active");
+				answers.style.maxHeight = null;
+			}
+		});
+
+		if (icon.classList.contains("active")) {
+			icon.classList.remove("active");
+			answer.style.maxHeight = null;
+		} else {
+			icon.classList.add("active");
+			answer.style.maxHeight = answer.scrollHeight + "px";
+			// console.log(answer.innerText);
+		}
+	});
+});
