b0aa9de020b6117125674a3e11002c96a0696aa8
shubhamCda <shubhambodalkar281@gmail.com>
2025-08-23 13:19:02 +0530

    Add accordian css/script files

--- accordian/script.js
+++ accordian/script.js
@@ -1,3 +1,20 @@
 const accordian = document.querySelectorAll(".accordian");
 
 console.log(accordian);
+
+accordian.forEach((accord) => {
+  let icon = accord.querySelector(".icon");
+  let answer = accord.querySelector(".answer");
+
+  accord.addEventListener("click", () => {
+    if (icon.classList.contains("active")) {
+      icon.classList.remove("active");
+      answer.style.maxHeight = null;
+      
+    } else {
+      icon.classList.add("active");
+      answer.style.maxHeight = answer.scrollHeight + "px";
+      // console.log(answer.innerText);
+    }
+  })
+})
